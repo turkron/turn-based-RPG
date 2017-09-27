ea5a108be833ea465321fd51e6ab100ae9356273
turkron <nathandkendall@hotmail.com>
2017-09-27 22:23:33 +0100

    end of night 1

---
+++ Assets/Scripts/attackTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class used to animate the current sprite towards another, play an animation and return back to its original position.
+
+public class attackTarget : MonoBehaviour {
+
+	public GameObject target;
+	public string attackType;
+	public float fightXPos;
+	public float fightYPos;
+
+	//need to remember to reference the script Lerp of this object. 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
---
+++ Assets/Scripts/stats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this class will hold all of the stats needed to create a new object. it must be completed in order to be used.
+
+public class stats : MonoBehaviour {
+
+	public int attack;
+	public int defense;
+	public int speed;
+	public int magic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
